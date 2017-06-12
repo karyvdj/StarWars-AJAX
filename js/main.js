eaf7d460b1874ac3b2e30fa6856be4bec8c2cc0d
karyvdj <kaina51@hotmail.com>
2017-06-12 11:11:32 -0500

    Version detalles de personajes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var cargarPagina = function () {
   cargarPersonajes();
+  $(document).on("click", ".personaje", mostrarDellatePersonaje)
 };
 
 var cargarPersonajes = function () {
@@ -14,16 +15,33 @@ var cargarPersonajes = function () {
 
 var mostrarTotalPersonajes = function (total){
   $("#total").text(total);
-}
+};
 
 var mostrarPersonajes = function (personajes) {
   var ul = $("#personajes");
   personajes.forEach(function (personaje) {
      var li = $("<li/>");
+     li.addClass("personaje");
+     li.attr("data-url", personaje.homeworld);
      li.text(personaje.name + " -  " + personaje.height);
      ul.append(li);
   });
-}
+};
+var pantillaPlaneta = "<h2>Planeta</h2>"+
+"<p><strong>Nombre: </strong>__nombre__</p>"+
+"<p><strong>Clima: </strong>__clima__</p>";
+
+var mostrarDellatePersonaje = function () {
+    var url = $(this).data("url");
+    var planetaContenedor = $("#planeta");
+    $.getJSON(url , function (response) {
+      planetaContenedor.html(
+        pantillaPlaneta.replace("__nombre__", response.name)
+        .replace("__clima__", response.climate)
+      );
+    });
+};
+
 $(document).ready(cargarPagina);
 
 // -----------Metodo anterior $.ajax--------------
