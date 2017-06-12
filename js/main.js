df0cab13ae50e5dfd4ef8eef36272c69f88b417e
karyvdj <kaina51@hotmail.com>
2017-06-12 10:13:54 -0500

    Version $.get

--- js/main.js
+++ js/main.js
@@ -3,29 +3,30 @@ var cargarPagina = function () {
 };
 
 var cargarPersonajes = function () {
-  $.ajax("http://swapi.co/api/people/",{
-    method: "GET",
-    dataType: "json",
-    success: function (response) {
-      var personajes = response.results;
-      $("#total").text(response.count);
-      var ul = $("#personajes");
-
-      personajes.forEach(function (personaje) {
-        var li = $("<li/>");
-        li.text(personaje.name + " -  " + personaje.height);
-        ul.append(li);
-      });
-    },
-    error: function (error) {
-      console.log("error", error);
-    }
+  var url = "http://swapi.co/api/people/";
+  $.get(url,function (response) {
+    var personajes = response.results;
+    var total = response.count;
+    mostrarTotalPersonajes(total);
+    mostrarPersonajes(personajes);
   });
 };
 
+var mostrarTotalPersonajes = function (total){
+  $("#total").text(total);
+}
+
+var mostrarPersonajes = function (personajes) {
+  var ul = $("#personajes");
+  personajes.forEach(function (personaje) {
+     var li = $("<li/>");
+     li.text(personaje.name + " -  " + personaje.height);
+     ul.append(li);
+  });
+}
 $(document).ready(cargarPagina);
 
-// -----------Metodo anterior---------
+// -----------Metodo anterior $.ajax--------------
 // var cargarPersonajes = function () {
 //   $.ajax("http://swapi.co/api/people/",{
 //     method: "GET",
